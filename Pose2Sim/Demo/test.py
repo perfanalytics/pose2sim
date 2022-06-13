006246a87b1e9a9e98c634436f86e8cf1c2fcff9
David PAGNON <contact@david-pagnon.com>
2022-06-13 11:43:37 +0200

    test

--- Pose2Sim/Demo/test.py
+++ Pose2Sim/Demo/test.py
@@ -1,6 +1,9 @@
 import os
+print('1')
 from Pose2Sim import Pose2Sim
+print('2')
 Pose2Sim.calibrateCams(os.path.join('User', 'test.toml'))
+print('3')
 Pose2Sim.track2D(os.path.join('User', 'test.toml'))
 Pose2Sim.triangulate3D(os.path.join('User', 'test.toml'))
 Pose2Sim.filter3D(os.path.join('User', 'test.toml'))
