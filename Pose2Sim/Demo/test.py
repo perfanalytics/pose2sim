5df8a14815dfb10e16fc0858a666aba488559114
David PAGNON <contact@david-pagnon.com>
2022-06-13 14:33:15 +0200

    Update test.py

--- Pose2Sim/Demo/test.py
+++ Pose2Sim/Demo/test.py
@@ -1,6 +1,6 @@
 import os
 
-def workflow_test():
+def test_workflow():
   from Pose2Sim import Pose2Sim
   Pose2Sim.calibrateCams(os.path.join('User', 'test.toml'))
   Pose2Sim.track2D(os.path.join('User', 'test.toml'))
