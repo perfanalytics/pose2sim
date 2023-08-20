2a60c2ee445938e329816bfd2636a2701088d828
davidpagnon <contact@david-pagnon.com>
2023-08-21 01:27:01 +0200

    update test

--- Pose2Sim/Demo/test.py
+++ Pose2Sim/Demo/test.py
@@ -2,7 +2,7 @@ import os
 
 def test_workflow():
   from Pose2Sim import Pose2Sim
-  Pose2Sim.calibrateCams(os.path.join('User', 'test.toml'))
-  Pose2Sim.track2D(os.path.join('User', 'test.toml'))
-  Pose2Sim.triangulate3D(os.path.join('User', 'test.toml'))
-  Pose2Sim.filter3D(os.path.join('User', 'test.toml'))
+  Pose2Sim.calibration(os.path.join('User', 'test.toml'))
+  Pose2Sim.personAssociation(os.path.join('User', 'test.toml'))
+  Pose2Sim.triangulation(os.path.join('User', 'test.toml'))
+  Pose2Sim.filtering(os.path.join('User', 'test.toml'))
