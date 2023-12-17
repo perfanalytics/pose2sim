f06d385d572baf7970674bb79e148bc1073734e9
davidpagnon <contact@david-pagnon.com>
2023-12-17 20:15:22 +0100

    raw string in test.py

--- Pose2Sim/Demo/S00_Demo_Session/test.py
+++ Pose2Sim/Demo/S00_Demo_Session/test.py
@@ -8,7 +8,7 @@ def test_workflow():
   config_dict.get("project").update({"project_dir":"."})
   Pose2Sim.calibration(config_dict)
   
-  config_dict.get("project").update({"project_dir":"S00_P00_Participant\S00_P00_T00_StaticTrial"})
+  config_dict.get("project").update({"project_dir":r"S00_P00_Participant\S00_P00_T00_StaticTrial"})
   config_dict['filtering']['display_figures'] = False
   # Pose2Sim.poseEstimation(config_dict)
   # Pose2Sim.synchronization(config_dict)
@@ -17,7 +17,7 @@ def test_workflow():
   Pose2Sim.filtering(config_dict)
   # Pose2Sim.kinematics(config_dict)
 
-  # config_dict.get("project").update({"project_dir":"S00_P00_Participant\S00_P00_T00_BalancingTrial"})
+  # config_dict.get("project").update({"project_dir":r"S00_P00_Participant\S00_P00_T00_BalancingTrial"})
   # config_dict['filtering']['display_figures'] = False
   # # Pose2Sim.poseEstimation(config_dict)
   # # Pose2Sim.synchronization(config_dict)
