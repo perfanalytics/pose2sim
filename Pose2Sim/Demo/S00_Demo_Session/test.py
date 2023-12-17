19d27ab74e79c7f05504786229258d48d35623e1
davidpagnon <contact@david-pagnon.com>
2023-12-17 21:03:01 +0100

    hopefully works on all platforms now

--- Pose2Sim/Demo/S00_Demo_Session/test.py
+++ Pose2Sim/Demo/S00_Demo_Session/test.py
@@ -1,15 +1,20 @@
+import os
 import toml
 
 def test_workflow():
   
   from Pose2Sim import Pose2Sim
   
+  # Calibration
   config_dict = toml.load('Config.toml')
   config_dict.get("project").update({"project_dir":"."})
   Pose2Sim.calibration(config_dict)
   
-  config_dict.get("project").update({"project_dir":r"S00_P00_Participant\S00_P00_T00_StaticTrial"})
+  # # Static trial
+  project_dir = os.path.join("S00_P00_Participant","S00_P00_T00_StaticTrial")
+  config_dict.get("project").update({"project_dir":project_dir})
   config_dict['filtering']['display_figures'] = False
+
   # Pose2Sim.poseEstimation(config_dict)
   # Pose2Sim.synchronization(config_dict)
   Pose2Sim.personAssociation(config_dict)
@@ -17,8 +22,12 @@ def test_workflow():
   Pose2Sim.filtering(config_dict)
   # Pose2Sim.kinematics(config_dict)
 
-  # config_dict.get("project").update({"project_dir":r"S00_P00_Participant\S00_P00_T00_BalancingTrial"})
+
+  # # Balancing trial
+  # project_dir = os.path.join("S00_P00_Participant","S00_P00_T01_BalancingTrial")
+  # config_dict.get("project").update({"project_dir":project_dir})
   # config_dict['filtering']['display_figures'] = False
+
   # # Pose2Sim.poseEstimation(config_dict)
   # # Pose2Sim.synchronization(config_dict)
   # Pose2Sim.personAssociation(config_dict)
