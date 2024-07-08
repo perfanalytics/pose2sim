b0530f58929af837dec75ffb59f108ee96b4406b
davidpagnon <contact@david-pagnon.com>
2024-07-09 00:47:05 +0200

    deactivated display for CI test of pose detection

--- Pose2Sim/tests.py
+++ Pose2Sim/tests.py
@@ -96,6 +96,7 @@ class TestWorkflow(unittest.TestCase):
         config_dict.get("project").update({"project_dir":project_dir})
         config_dict.get("project").update({"multi_person":False})
         config_dict.get("pose").update({"mode":'lightweight'})
+        config_dict.get("pose").update({"display_detection":False})
         config_dict.get("synchronization").update({"display_sync_plots":False})
         config_dict['filtering']['display_figures'] = False
 
@@ -119,6 +120,7 @@ class TestWorkflow(unittest.TestCase):
         config_dict.get("project").update({"project_dir":project_dir})
         config_dict.get("project").update({"multi_person":True})
         config_dict.get("pose").update({"mode":'lightweight'})
+        config_dict.get("pose").update({"display_detection":False})
         config_dict.get("synchronization").update({"display_sync_plots":False})
         config_dict.get("markerAugmentation").update({"participant_height":[1.21, 1.72]})
         config_dict.get("markerAugmentation").update({"participant_mass":[25.0, 70.0]})
