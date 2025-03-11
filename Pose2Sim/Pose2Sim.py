09488fd1e9a4a554dd6f0ea25bdf4120039f5ccd
Laizo <aureliencoppee@gmail.com>
2025-03-11 07:55:32 +0100

    fix

--- Pose2Sim/Pose2Sim.py
+++ Pose2Sim/Pose2Sim.py
@@ -183,13 +183,13 @@ class Pose2SimPipeline:
     def calibration(self):
         from Pose2Sim.calibration import calibrate_cams_all
         config_dict = self.config_dicts[0]
-        project_dir = os.path.realpath(config_dict["project"]["project_dir"])
+        config_dict.get("project").update({"session_dir": self.session_dir})
 
         try:
             calib_dirs = [
-                os.path.join(project_dir, c)
-                for c in os.listdir(project_dir)
-                if os.path.isdir(os.path.join(project_dir, c)) and 'calib' in c.lower()
+                os.path.join(self.session_dir, c)
+                for c in os.listdir(self.session_dir)
+                if os.path.isdir(os.path.join(self.session_dir, c)) and 'calib' in c.lower()
             ]
             calib_dir = calib_dirs[0]
         except IndexError:
