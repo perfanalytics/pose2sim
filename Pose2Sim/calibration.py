1b561143fdeecd7f0f59155865bc34dc19cb67c8
David PAGNON <contact@david-pagnon.com>
2026-01-14 09:48:22 +0100

    removed res from read_qca, as resolution is computed later

--- Pose2Sim/calibration.py
+++ Pose2Sim/calibration.py
@@ -125,23 +125,19 @@ def read_qca(qca_path, binning_factor):
 
     root = etree.parse(qca_path).getroot()
     ret, C, S, D, K, R, T = [], [], [], [], [], [], []
-    res = []
     vid_id = []
     
     # Camera name
     for i, tag in enumerate(root.findall('cameras/camera')):
         ret += [float(tag.attrib.get('avg-residual'))]
         C += [tag.attrib.get('serial')]
-        res += [int(tag.attrib.get('video_resolution')[:-1]) if tag.attrib.get('video_resolution') not in (None, "N/A") else 1080]
         if any(model in tag.attrib.get('model', '').lower() for model in ["video", "none"]):
             vid_id += [i]
     
     # Image size
     for i, tag in enumerate(root.findall('cameras/camera/fov_video')):
-        w = (float(tag.attrib.get('right')) - float(tag.attrib.get('left')) +1) /binning_factor \
-            / (1080/res[i]) 
-        h = (float(tag.attrib.get('bottom')) - float(tag.attrib.get('top')) +1) /binning_factor \
-            / (1080/res[i])
+        w = (float(tag.attrib.get('right')) - float(tag.attrib.get('left')) +1) /binning_factor
+        h = (float(tag.attrib.get('bottom')) - float(tag.attrib.get('top')) +1) /binning_factor
         S += [[w, h]]
     
     # Intrinsic parameters: distorsion and intrinsic matrix
@@ -152,16 +148,12 @@ def read_qca(qca_path, binning_factor):
         p2 = float(tag.get('tangentalDistortion2'))/64/binning_factor
         D+= [np.array([k1, k2, p1, p2])]
         
-        fu = float(tag.get('focalLengthU'))/64/binning_factor \
-            / (1080/res[i])
-        fv = float(tag.get('focalLengthV'))/64/binning_factor \
-            / (1080/res[i])
-        cu = (float(tag.get('centerPointU'))/64/binning_factor \
-            - float(root.findall('cameras/camera/fov_video')[i].attrib.get('left'))) \
-            / (1080/res[i])
-        cv = (float(tag.get('centerPointV'))/64/binning_factor \
-            - float(root.findall('cameras/camera/fov_video')[i].attrib.get('top'))) \
-            / (1080/res[i])
+        fu = float(tag.get('focalLengthU'))/64/binning_factor
+        fv = float(tag.get('focalLengthV'))/64/binning_factor
+        cu = float(tag.get('centerPointU'))/64/binning_factor \
+            - float(root.findall('cameras/camera/fov_video')[i].attrib.get('left'))
+        cv = float(tag.get('centerPointV'))/64/binning_factor \
+            - float(root.findall('cameras/camera/fov_video')[i].attrib.get('top'))
         K += [np.array([fu, 0., cu, 0., fv, cv, 0., 0., 1.]).reshape(3,3)]
 
     # Extrinsic parameters: rotation matrix and translation vector
